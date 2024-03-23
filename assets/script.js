e8dd621d5e7de3c31c9968e0d18c191f97524244
Justin Delgado <delgadojustin100@yahoo.com>
2024-03-23 14:57:22 -0400

    made the app fully functional

--- assets/script.js
+++ assets/script.js
@@ -2,57 +2,70 @@ const results = document.getElementById('output');
 const addBtn = document.querySelector("#add")
 const subBtn = document.querySelector('#sub')
 const numberBtn = document.querySelectorAll('#numbers .number-btn');
+const equalBtn = document.querySelector('#equalbtn')
 
 addBtn.onclick = addStuff;
 let currValue = 0
+let num1 = 0
+let num2 = 0
 let clickCount = 0
+let currentOperation = null; // To track the current operation
+
+
 const buttonText = numberBtn.innerHTML
 
 
 numberBtn.forEach(button => {
+   
    button.addEventListener("click", handleClick )
+
    })
 
 function handleClick() {
-      currValue += parseInt(this.textContent)
-       console.log(currValue);
+   if (results.innerText === "Click second number") {
+      results.innerText = "";
+  }
+   clickCount++;
+   if(clickCount == 1) {
+      results.innerText =""
+   }
+
+      currValue = parseInt(this.innerHTML)
+      
        
-       clickCount++;
-       results.innerText = " "
        results.innerText += currValue
-
-
-       if (clickCount === 1) {
-         numberBtn.forEach(button => {
-            button.removeEventListener("click", handleClick)
-            addBtn.style.boxShadow = "5px 2px 5px red";
-         })
-       }
 }
 
 function addStuff () {
-   
+  num1 = parseInt(results.innerText)
+  console.log(num1);
+currentOperation = ("add")
+results.innerText = "Click second number";
+
 }
 
 function subStuff() {
-   results.innerText--
+
 }
 
+function equalStuff() {
+
+   // to make it equal to the numbers that are clicked
+
+   num2 = parseInt(results.innerText)
+   let result;
+   if (currentOperation === "add")  {
+      result = num1 + num2
+      
+   }
+   results.innerText = result
+}
+
+
+
+
 subBtn.onclick = subStuff;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+equalBtn.onclick = equalStuff;
 
 
 
