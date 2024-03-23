c9d2be5acb6c084dbadd27d39a3b7501fc4f7fb3
Justin Delgado <delgadojustin100@yahoo.com>
2024-03-23 01:08:21 -0400

    making the event listener

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const results = document.getElementById('equalbtn');
+const addbtn = document.querySelector("#add")
+
+
+
+addbtn.onclick = addStuff;
+
+function addStuff () {
+   console.log("yes");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
